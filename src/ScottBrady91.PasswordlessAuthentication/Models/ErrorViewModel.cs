795a36762bcbaa6dbd7b6797f471e4a264e6c857
Scott Brady <scottbrady91@outlook.com>
2018-03-02 17:30:59 +0000

    Cleanup

--- src/ScottBrady91.PasswordlessAuthentication/Models/ErrorViewModel.cs
+++ src/ScottBrady91.PasswordlessAuthentication/Models/ErrorViewModel.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace ScottBrady91.PasswordlessAuthentication.Models
 {
     public class ErrorViewModel
--- src/ScottBrady91.PasswordlessAuthentication/Startup.cs
+++ src/ScottBrady91.PasswordlessAuthentication/Startup.cs
@@ -1,27 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ScottBrady91.PasswordlessAuthentication
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
@@ -34,29 +20,14 @@ namespace ScottBrady91.PasswordlessAuthentication
 
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseBrowserLink();
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
+            app.UseDeveloperExceptionPage();
 
             app.UseAuthentication();
 
             app.UseStaticFiles();
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
